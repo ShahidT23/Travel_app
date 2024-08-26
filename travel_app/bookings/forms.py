6502b297d91bc786f18a717923b9722bcb5f8016
chavan-sanika <chavansanika01@gmail.com>
2024-08-26 19:02:59 +0530

    Changes in Flight Page as Flight Input Field And Refund container box

--- travel_app/bookings/forms.py
+++ travel_app/bookings/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+import datetime
 class FlightSearchForm(forms.Form):
     departure_city = forms.CharField(max_length=50,label="Departure City",
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Search for cities..'})
@@ -7,14 +7,26 @@ class FlightSearchForm(forms.Form):
     arrival_city = forms.CharField(max_length=50, label="Arrival City",
            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Search for cities..'})
     )
-    departure_date = forms.DateField(widget=forms.SelectDateWidget, label="Departure Date"
+      # Set current date as initial value for departure_date
+    departure_date = forms.DateField(
+        widget=forms.SelectDateWidget(attrs={'class': 'form-control','type':'date'}), 
+        label="Departure Date",
+        initial=datetime.date.today  # This sets the current date as default
     )
-    return_date = forms.DateField(widget=forms.SelectDateWidget, label="Return Date", required=False
+    
+    return_date = forms.DateField(
+        widget=forms.SelectDateWidget(attrs={'class': 'form-control','type':'date'}), 
+        label="Return Date", 
+        required=False
     )
     travellers = forms.IntegerField(min_value=1, label="Traveller",
     widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '1'})
     )
-    travel_class = forms.ChoiceField(choices=[('economy', 'Economy'), ('business', 'Business'), ('first', 'First Class')], label="Class")
+    travel_class = forms.ChoiceField(choices=[('economy', 'Economy'), ('business', 'Business'), ('first', 'First Class')],label="Class",widget=forms.Select(attrs={'class': 'form-control'})
+   )
+
+
+   
 
 class HotelSearchForm(forms.Form):
     city = forms.CharField(max_length=100, label="City")
